f244c71c82cd4f8893e318352f7f455bbf15c254
Gigi Minova <minova1995@gmail.com>
2019-01-27 23:09:16 +0000

    triangle of numbers solved

---
+++ 1_8TriangleOfNumbers/1_8TriangleOfNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Write a program, which receives a number – n, 
+ * and prints a triangle from 1 to n as in the examples.
+Constraints: n will be in the interval [1...20].
+
+ EXAMPLE
+____________________________
+_INPUT________________OUTPUT
+   3                  1
+                      2 2
+                      3 3 3
+_____________________________
+   5                  1
+                      2 2
+                      3 3 3
+                      4 4 4 4
+                      5 5 5 5 5
+ _____________________________
+   6                  1
+                      2 2
+                      3 3 3
+                      4 4 4 4
+                      5 5 5 5 5
+                      6 6 6 6 6 6
+_______________________________                                          
+ */
+using System;
+
+namespace _1_8TriangleOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
